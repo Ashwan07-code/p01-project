1bacade2b3a2d4ede2a8041d7726fe2a8a1508be
Bilalt66 <302773688@student.rocmondriaan.nl>
2025-11-03 14:01:15 +0100

    heb leeftijd input en naam input knop gemaakt en mooi gemaakt

--- homepagina.js
+++ homepagina.js
@@ -9,3 +9,15 @@
       document.getElementById("QuizBtn3").addEventListener("click", function() {
         window.location.href = "Ashwan/index.html";
       });
+       document.getElementById("verstuur").addEventListener("click", function() {
+    
+      const naam = document.getElementById("naam").value;
+      const leeftijd = document.getElementById("leeftijd").value;
+
+      console.log("Naam:", naam);
+      console.log("Leeftijd:", leeftijd);
+
+    
+      alert(`Hallo ${naam}! Welkom in de quiz.`);
+    });
+  
